c3b295b1a772d42878f7c5df4112e0fca1f595ec
xinghouliu <xinghouliu@datamesh.com>
2019-05-31 19:05:31 +0800

    init over

---
+++ common-mysql/src/main/java/com/steve/mysql/common/QueryParam.java
@@ -0,0 +1,10 @@
+package com.steve.mysql.common;
+
+import java.io.Serializable;
+
+
+public interface QueryParam extends Serializable {
+
+    boolean hasData();
+
+}
---
+++ common-mysql/src/main/java/com/steve/mysql/exception/DbServiceException.java
@@ -0,0 +1,12 @@
+package com.steve.mysql.exception;
+
+
+import com.steve.exception.BaseException;
+
+public class DbServiceException extends BaseException {
+
+    public DbServiceException(String message) {
+        super(message);
+    }
+
+}
